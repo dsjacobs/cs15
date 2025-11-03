2123f4fefd85b18b08e73084d54c39b6bae7f3df
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-02 19:56:21 -0500

    std endl

--- typewriter/unit_tests.cpp
+++ typewriter/unit_tests.cpp
@@ -13,33 +13,33 @@
 #include <iostream>
 
 void isEmpty_test() {
-    std::cout << "Test 1: isEmpty" << endl;
+    std::cout << "Test 1: isEmpty" << std::endl;
     ActionStack my_stack;
     assert(my_stack.isEmpty());
 }
 
 void isEmpty_false() {
-    std::cout << "Test 2: is not Empty" << endl;
+    std::cout << "Test 2: is not Empty" << std::endl;
     ActionStack my_stack;
     my_stack.push('j',false,0,0);
     assert(not my_stack.isEmpty());
 }
 
 void size_zero() {
-    std::cout << "Test 3: size 0" << endl;
+    std::cout << "Test 3: size 0" << std::endl;
     ActionStack my_stack;
     assert(my_stack.size()==0);
 }
 
 void size_one() {
-    std::cout << "Test 4: size 1" << endl;
+    std::cout << "Test 4: size 1" << std::endl;
     ActionStack my_stack;
     my_stack.push('j',false,0,0);
     assert(my_stack.size()==1);
 }
 
 void size_many() {
-    std::cout << "Test 5: size many" << endl;
+    std::cout << "Test 5: size many" << std::endl;
     ActionStack my_stack;
     my_stack.push('j',false,0,0);
     my_stack.push('k',false,0,1);
