3d3b472cfcec38a65bd72a59b9dd9acfb27cf1d4
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-02 19:27:20 -0500

    remove parenthesis

--- typewriter/unit_tests.cpp
+++ typewriter/unit_tests.cpp
@@ -12,11 +12,11 @@
 #include <cassert>
 
 void isEmpty_test() {
-    ActionStack my_stack();
+    ActionStack my_stack;
     assert(my_stack.isEmpty());
 }
 
 int main() {
-    isEmptcaty_test();
+    isEmpty_test();
     return 0;
 }
