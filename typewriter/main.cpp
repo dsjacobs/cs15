37884eee3fd1bc4a73aef5dc8caecaeca3151507
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-04 14:16:17 -0500

    logging in main

--- typewriter/main.cpp
+++ typewriter/main.cpp
@@ -16,17 +16,20 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc > 2) {
+    if (argc > 2 or argc == 0) {
         std::cerr << "Invalid number of arguments";
     }
     std::string text_filename = argv[1];
     std::string log_filename = "";
+    std::cout << text_filename << std::endl;
+    std::cout << log_filename << std::endl;
     Editor my_editor(text_filename);
+    std::cout << "was able to make an instance" << std::endl;
     if (argc == 2) {
         log_filename = argv[2];
     }
-    else {
-        my_editor.print_inp_file(text_filename, cout);     
-    }
+    std::cout << "printing input file" << std::endl;
+    my_editor.print_inp_file(text_filename, cout);     
+    
     return 0;
 }
