8ecb152e0ca34622d6259b4c2cb3dd366e2bd128
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-08 20:55:33 -0500

    col instead of line

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -120,7 +120,7 @@ void Editor::ascii(int c) {
     char c_char = static_cast<char>(c);
     curTextLines[cursorLine] = preCursorText + c_char + postCursorText;
     undoStack.push(c_char,false,cursorLine,cursorCol);
-    cursorLine++;
+    cursorCol++;
 };
 
 void Editor::command_mode() {
