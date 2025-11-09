4b6d79deb615a6314a4b005fbbc0e87beca9365b
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-08 20:37:19 -0500

    can i double declare it

--- typewriter/main.cpp
+++ typewriter/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
     Editor my_editor(text_filename);
     if (argc == 2) {
         log_filename = argv[2];
-        my_editor.add_logfile(log_filename);
+        Editor my_editor(text_filename, log_filename);
     }
     my_editor.run();  
     
