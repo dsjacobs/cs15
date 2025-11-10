0b88e13c759f50c48f779df0c7ffce4ad7a87e76
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-09 21:27:09 -0500

    less or equal to

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -210,7 +210,7 @@ void Editor::new_line() {
     size_t curLine = cursorLine;
     std::vector<std::string> latter_half;
     if (curLine < curTextLines.size()) {
-        for (size_t i = curLine; i < curTextLines.size(); i++) {
+        for (size_t i = curLine; i <= curTextLines.size(); i++) {
             latter_half.push_back(curTextLines[i]);
             curTextLines.pop_back();
         }
