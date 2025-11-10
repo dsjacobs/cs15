a0e1dc868b54e9065fed0e7b18a5d43f00060e81
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-09 23:06:12 -0500

    in between

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -187,8 +187,9 @@ void Editor::backspace() {
     // middle of a row
     if (cursorCol != 0) 
     {
-        undoStack.push(curChar,true,cursorLine,cursorCol);
         char curChar = curTextLines[cursorLine][cursorCol];
+                undoStack.push(curChar,true,cursorLine,cursorCol);
+
         delete_char(cursorLine, cursorCol); 
         cursorCol--;
     }
