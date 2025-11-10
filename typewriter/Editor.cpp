35cfd6f38ab2d254b5fe72d28a71ba3dddcee9f5
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-09 23:24:24 -0500

    size t instead of int

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -179,7 +179,7 @@ void Editor::command_mode() {
     }
 };
 
-void Editor::delete_char(int line, int col) {
+void Editor::delete_char(size_t line, size_t col) {
    std::string line_end = "";
    if (col < lineLength(line) - 1) {
         line_end = post_character(line, col+1); 
