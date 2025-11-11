a9c325674204c8805a0209108da35be6bb63c64c
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-10 23:02:38 -0500

    a simpler way to move everything one down

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -231,7 +231,6 @@ void Editor::backspace() {
 void Editor::new_line() {
     size_t curLine = cursorLine;
     size_t curLineLength = lineLength(cursorLine);
-    std::vector<std::string> latter_half;
     // very end of file
     if (curLine==numLines - 1 and cursorCol == curLineLength) {
         curTextLines.push_back("");
@@ -239,12 +238,8 @@ void Editor::new_line() {
     else {
         std::string postCursorText = post_character(cursorLine, cursorCol);
         curTextLines[curLine] = pre_character(cursorLine, cursorCol); 
-        for (size_t i = curLine+1; i < numLines; i++) {
-            latter_half.push_back(curTextLines[i]);
-        }
-        curTextLines.push_back(postCursorText);
-        for (size_t i = 0; i < latter_half.size(); i++) {
-            curTextLines.push_back(latter_half[i]);
+        for (size_t i = numLines; i > curLine; i--) {
+            curTextLines[i+1] = curTextLines[i];
         }
     }
     cursorCol = 0;
