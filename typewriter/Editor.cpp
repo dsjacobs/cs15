ace88fef5c77c0877bb3bbc500c879c8669fe493
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-11 23:10:42 -0500

    typo

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -84,7 +84,7 @@ void Editor::determine_next(int c) {
 }
 
 std::ifstream Editor::read_file_open_stream(std::string text_file) {
-        std::ifstream myifsstream;
+        std::ifstream myifstream;
         myifstream.open(text_file);
         return myifstream;
 }
