90cbd3e0745658f782facd6e8ce4770d691dc8ca
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-11-13 23:52:58 -0500

    the reader or constructor

--- typewriter/Editor.cpp
+++ typewriter/Editor.cpp
@@ -33,7 +33,7 @@ void Editor::constructor_helper(std::string filename) {
     cursorLine = 0;
     numLines = 0;
     text_filename = filename;
-    std::ifstream myifstream = read_file_open_stream(filename);
+    std::ifstream myifstream; // = read_file_open_stream(filename);
     curTextLines = std::vector<std::string>(); 
     std::string ifstream_string;
     // while (getline (myifstream, ifstream_string)) {
